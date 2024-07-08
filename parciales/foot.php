b03882aad651e8f722fc3f4d41ae899607e94fa3
sergiovivart <sevivarte@gmail.com>
2024-07-08 12:26:04 -0700

    agregado el nuevo codigo par mi pagi wen estatica

--- parciales/foot.php
+++
@@ -1,9 +0,0 @@
-    <script  src="js/vendor/jquery.js"></script>
-    <script  src="js/vendor/foundation.js"></script>
-    <script  src="js/particles.js"></script>
-    <!-- <script src="js/typed.js"></script> -->
-    <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11"></script>
-
- <script>
-      $(document).foundation();
-  </script>
